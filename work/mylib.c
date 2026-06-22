837a9d3458423f5514692cab2e7cb7fd0c48cff1
justVako <vdand23@freeuni.edu.ge>
2026-06-22 19:32:19 +0400

    add

--- work/mylib.c
+++ work/mylib.c
@@ -13,3 +13,12 @@ int sub(int a, int b) {
     return c;
 }
 
+int is_prime(int n) {
+    if (n < 2) return 0;
+    if (n % 2 == 0) return n == 2;
+    for (int d = 3; (int)d * d <= n; d += 2) {
+        if (n % d == 0) return 0;
+    }
+    return 1;
+}
+
