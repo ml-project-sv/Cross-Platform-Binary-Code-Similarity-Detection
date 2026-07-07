c2591a315e3debc91d02b33adb436f172bfc11fb
SandroBolkvadze <sbolk23@freeuni.edu.ge>
2026-07-07 10:48:40 +0400

    update

--- setup.sh
+++ setup.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get update
-
-apt-get install -y gcc gcc-multilib build-essential wget unzip python3-pip
+apt-get install -y gcc gcc-multilib g++-multilib libc6-dev-i386 build-essential wget unzip python3-pip
 apt-get install -y gcc-arm-linux-gnueabi gcc-mips-linux-gnu
-
 pip install angr
+apt-get update
 
 gcc -m32 -dumpmachine
 arm-linux-gnueabi-gcc -dumpmachine
