f9adfd9cc6c77413a79f0819fc0e5bd6e0d9879d
SandroBolkvadze <sbolk23@freeuni.edu.ge>
2026-07-07 11:47:01 +0400

    update

--- tmp/main.c
+++ tmp/main.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include "math.h"
 
+void foo() {
+    printf("foo\n");
+}
+
+void bar() {
+    printf("bar\n");
+}
+
 int main() {
+    bar();
     printf("%d\n", add(1, 2));
     return 0;
 }
